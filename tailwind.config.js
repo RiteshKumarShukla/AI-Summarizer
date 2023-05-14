9324197a65bd0a1ba1b57cc77eee92ef956756d4
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2023-05-14 13:05:48 +0530

    initial setup completed

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+
+// tailwind.config.css
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content : [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      fontFamily:{
+        satoshi:["Satoshi", "sans-serif"],
+        inter : ["Inter", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
